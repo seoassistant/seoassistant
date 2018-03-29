cfecf381314d744d2b7d8990429a69f448cb76fd
Jota Teles <josetelesmaciel@gmail.com>
2018-03-28 22:21:41 -0300

    NO-ISSUE - adds babel to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
 var path = path = require("path");
 
 module.exports = {
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel",
+            query: {
+                presets: ["es2015"]
+            }
+        }],
+    },
     entry: path.join(__dirname, "src", "seoassistant","seoassistant.js"),
     output: {
         path: path.join(__dirname, "dist"),
